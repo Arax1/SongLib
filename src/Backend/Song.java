f433fa99bcefd353a4223cae20b5b106342f5db8
Sammy Berger <sammy27berger@gmail.com>
2019-02-13 15:53:28 -0500

    Final toString

--- src/Backend/Song.java
+++ src/Backend/Song.java
@@ -27,6 +27,6 @@ public class Song implements Comparable<Song> {
 	}
 	
 	public String toString() {
-		return name + ", by " + artist + " in " + album + " [" + year + "]";
+		return name + ", by " + artist;
 	}
 }
